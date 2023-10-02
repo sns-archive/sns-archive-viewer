be62207a59087e8788e66901f10a17c9c9644a30
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-10-02 23:24:55 +0900

    RSpecの修正

--- backend/spec/factories/users.rb
+++ backend/spec/factories/users.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                             :bigint           not null, primary key
+#  email(ユーザーのEmailアドレス) :string(255)      not null
+#  password(ユーザーのpassword)   :string(255)      not null
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#
 FactoryBot.define do
   factory :user do
-    name { 'hogehoge' }
     email { 'hogehoge@example.com' }
+    password { 'sample_password' }
   end
 end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                             :bigint           not null, primary key
+#  email(ユーザーのEmailアドレス) :string(255)      not null
+#  password(ユーザーのpassword)   :string(255)      not null
+#  created_at                     :datetime         not null
+#  updated_at                     :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
