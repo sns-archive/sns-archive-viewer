0d797f92e00c8b091da53606710ebecb89efd93e
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-10-02 23:55:02 +0900

    不要なmigrationファイルの削除とRSpecの修正

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -8,8 +8,6 @@
 #  created_at                     :datetime         not null
 #  updated_at                     :datetime         not null
 #
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
   it "is valid with valid attributes" do
     user = build(:user)
