b29bba94f07c4954705ddec4993bfc92d570ceb5
Kaito1290 <90958196+Kaito1290@users.noreply.github.com>
2023-10-21 16:46:10 +0900

    RSpecを修正

--- backend/spec/models/memo_spec.rb
+++ backend/spec/models/memo_spec.rb
@@ -21,32 +21,48 @@ RSpec.describe Memo, type: :model do
     context 'titleが空文字の場合' do
       let(:memo) { build(:memo, title: '') }
 
-      it 'valid?メソッドがfalseを返す' do
-        expect(memo.valid?).to eq false
+      it 'valid?メソッドがfalseを返し、errorsに「タイトルを入力してください」と格納される' do
+        aggregate_failures do
+          result = memo.valid?
+          expect(result).to eq false
+          expect(memo.errors.full_messages).to eq ['タイトルを入力してください']
+        end
       end
     end
 
     context 'titleがnilの場合' do
       let(:memo) { build(:memo, title: nil) }
 
-      it 'valid?メソッドがfalseを返す' do
-        expect(memo.valid?).to eq false
+      it 'valid?メソッドがfalseを返し、errorsに「タイトルを入力してください」と格納される' do
+        aggregate_failures do
+          result = memo.valid?
+          expect(result).to eq false
+          expect(memo.errors.full_messages).to eq ['タイトルを入力してください']
+        end
       end
     end
 
     context 'contentが空文字の場合' do
       let(:memo) { build(:memo, content: '') }
 
-      it 'valid?メソッドがfalseを返す' do
-        expect(memo.valid?).to eq false
+      it 'valid?メソッドがfalseを返し、errorsに「コンテンツを入力してください」と格納される' do
+        aggregate_failures do
+          result =  memo.valid?
+          expect(result).to eq false
+          expect(memo.errors.full_messages).to eq ['コンテンツを入力してください']
+        end
       end
     end
 
     context 'contentがnilの場合' do
       let(:memo) { build(:memo, content: nil) }
 
-      it 'valid?メソッドがfalseを返す' do
-        expect(memo.valid?).to eq false
+      it 'valid?メソッドがfalseを返し、errorsに「コンテンツを入力してください」と格納される' do
+        aggregate_failures do
+          result = memo.valid?
+          expect(result).to eq false
+          expect(memo.errors.full_messages).to eq ['コンテンツを入力してください']
+        end
       end
     end
   end
