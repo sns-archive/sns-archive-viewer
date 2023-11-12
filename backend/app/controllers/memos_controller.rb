43856c1509204f37473fa36e0f52fde75d9188e1
日下陸矢 <rikuya.98@gmail.com>
2023-11-12 16:17:41 +0900

    ・Rubocopの実行

--- backend/app/controllers/memos_controller.rb
+++ backend/app/controllers/memos_controller.rb
@@ -1,17 +1,20 @@
-class MemosController < ApplicationController
-    def create
-        memo = Memo.new(memo_params)
-        # 成功した場合ステータスコードのみ返す
-        if memo.save
-            head :no_content
-            # 入力値が空の場合エラーメッセージをJSONで返す
-          else
-            render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity 
-          end
-    end
+# frozen_string_literal: true
 
-    private
-    def memo_params
-        params.require(:memo).permit( :title, :content)
+class MemosController < ApplicationController
+  def create
+    memo = Memo.new(memo_params)
+    # 成功した場合ステータスコードのみ返す
+    if memo.save
+      head :no_content
+    # 入力値が空の場合エラーメッセージをJSONで返す
+    else
+      render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:title, :content)
+  end
 end
