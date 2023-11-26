ff5de6e9f11933034c79825d28e1f7ae41c9aa97
日下陸矢 <rikuya.98@gmail.com>
2023-11-26 22:35:41 +0900

    ・memo_controllerの不要なコメントを削除

--- backend/app/controllers/memos_controller.rb
+++ backend/app/controllers/memos_controller.rb
@@ -3,10 +3,8 @@
 class MemosController < ApplicationController
   def create
     memo = Memo.new(memo_params)
-    # 成功した場合ステータスコードのみ返す
     if memo.save
       head :no_content
-    # 入力値が空の場合エラーメッセージをJSONで返す
     else
       render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity
     end
