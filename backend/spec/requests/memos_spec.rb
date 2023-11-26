2493c8a702d9bd06708899e89a5fd7977689f375
日下陸矢 <rikuya.98@gmail.com>
2023-11-26 22:38:33 +0900

    ・letの定義を各contextごとに設定。 ・バリデーションエラーのテストケースを１つに統一。 ・beforeを削除しパフォーマンスを向上。 ・rubocopを実行

--- backend/spec/requests/memos_spec.rb
+++ backend/spec/requests/memos_spec.rb
@@ -1,71 +1,34 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe 'Memos' do
   describe 'MemoAPI' do
-    let(:valid_memo_params) { attributes_for(:memo) }
-    let(:empty_memo_params) { { title: '', content: '' } }
-    let(:empty_title_memo_params) { { title: '', content: 'test_content' } }
-    let(:empty_content_memo_params) { { title: 'test_title', content: '' } }
-
     context 'タイトルとコンテンツが有効な場合' do
-      before do
-        post '/memos', params: { memo: valid_memo_params }
-      end
+      let(:valid_memo_params) { attributes_for(:memo) }
 
-      it 'レコードが追加される' do
-        expect { post '/memos', params: { memo: valid_memo_params } }.to change(Memo, :count).by(+1)
-      end
-
-      it '204のステータスが返ってくる。' do
-        expect(response).to have_http_status(:no_content)
+      it 'memoレコードが追加され、204になる' do
+        aggregate_failures do
+          expect { post '/memos', params: { memo: valid_memo_params } }.to change(Memo, :count).by(+1)
+          expect(response).to have_http_status(:no_content)
+        end
       end
 
       it 'リクエストボディが返される。' do
+        post '/memos', params: { memo: valid_memo_params }
         expect(response.body).to be_empty
       end
     end
 
-    context 'タイトルとコンテンツが空の場合' do
-      before do
-        post '/memos', params: { memo: empty_memo_params }
-      end
+    context 'バリデーションエラーになる場合' do
+      let(:empty_memo_params) { { title: '', content: '' } }
 
       it '422のステータスが返ってくる' do
+        post '/memos', params: { memo: empty_memo_params }
         expect(response).to have_http_status(:unprocessable_entity)
       end
 
       it 'response.bodyに「タイトルを入力してください,コンテンツを入力してください」が返ってくる。' do
-        expect(response.body).to eq('{"errors":["タイトルを入力してください","コンテンツを入力してください"]}')
-      end
-    end
-
-    context 'タイトルが空の場合' do
-      before do
-        post '/memos', params: { memo: empty_title_memo_params }
-      end
-
-      it '422のステータスが返ってくる' do
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it 'response.bodyに「タイトルを入力してください」が返ってくる。' do
-        expect(response.body).to eq('{"errors":["タイトルを入力してください"]}')
-      end
-    end
-
-    context 'コンテンツが空の場合' do
-      before do
-        post '/memos', params: { memo: empty_content_memo_params }
-      end
-
-      it '422のステータスが返ってくる' do
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
-
-      it 'response.bodyに「コンテンツを入力してください」が返ってくる。' do
-        expect(response.body).to eq('{"errors":["コンテンツを入力してください"]}')
+        post '/memos', params: { memo: empty_memo_params }
+        expect(response.parsed_body['errors']).to eq(%w[タイトルを入力してください コンテンツを入力してください])
       end
     end
   end
