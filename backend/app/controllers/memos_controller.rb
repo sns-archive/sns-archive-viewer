9024afd8a1919e92367dc30ac48ccb4412c36d17
日下陸矢 <rikuya.98@gmail.com>
2024-02-03 12:59:50 +0900

    ・indexとshowのコントローラーの追加 ・memo.findの処理を共通化

--- backend/app/controllers/memos_controller.rb
+++ backend/app/controllers/memos_controller.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 class MemosController < ApplicationController
+  before_action :find_memo, only: %i[show update destroy]
+
+  # GET /memos
+  def index
+    memos = Memo.all
+    render json: { memos: memos }, status: :ok
+  end
+
+  # GET /memos/:id
+  def show
+    render json: { memo: @memo }, status: :ok
+  end
+
   # POST /memos
   def create
     memo = Memo.new(memo_params)
@@ -13,24 +26,26 @@ class MemosController < ApplicationController
 
   # PUT /memos/:id
   def update
-    memo = Memo.find(params[:id])
-    if memo.update(update_memo_params)
+    if @memo.update(update_memo_params)
       head :no_content
     else
-      render json: { errors: memo.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: @memo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   # DELETE /memos/:id
   def destroy
-    memo = Memo.find(params[:id])
-    memo.destroy
+    @memo.destroy
     head :no_content
   rescue ActiveRecord::RecordNotFound => e
     render json: { message: e.message }, status: :not_found
   end
 
   private
+
+  def find_memo
+    @memo = Memo.find(params[:id])
+  end
 
   def memo_params
     params.require(:memo).permit(:title, :content)
