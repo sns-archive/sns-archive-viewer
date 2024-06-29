4334135d577f1e8f2027b437fcb42e97327dadab
taku12 <taku.enginner@gmail.com>
2024-06-29 12:59:52 +0900

    create seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do |n|
+    p "========== user create #{n+1}=========="
+    User.create!(
+        email: "example_#{n+1}@example.com",
+        password: "password"
+    )
+
+    p "========== memo create #{n+1}=========="
+    Memo.create!(
+        title: "title_#{n+1}",
+        content: "content_#{n+1}"
+    )
+
+    p "========== comment create #{n+1}=========="
+    Comment.create!(
+        memo_id: n+1,
+        content: "content_#{n+1}"
+    )
+end
