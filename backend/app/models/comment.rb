ce03e443dc37d0059545f91f14e287d3170f2d65
nova <173802000+nova818@users.noreply.github.com>
2024-07-19 08:47:29 +0900

    Revert "commentsテーブルのmemo_idに外部キー制約を付与するマイグレーションファイルを追加"
    This reverts commit 072cd4b040f60fb8ca06979d33197d6a78c42277.

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -8,15 +8,11 @@
 #  content(内容)   :text(65535)      not null
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
-#  memo_id(メモID) :bigint           not null
+#  memo_id(メモID) :integer          not null
 #
 # Indexes
 #
 #  index_comments_on_memo_id  (memo_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (memo_id => memos.id)
 #
 class Comment < ApplicationRecord
   validates :content, presence: true
--- backend/db/migrate/20240717012912_add_foreign_key_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToComments < ActiveRecord::Migration[7.0]
-  def change
-    change_column :comments, :memo_id, :bigint
-    add_foreign_key :comments, :memos
-  end
-end
