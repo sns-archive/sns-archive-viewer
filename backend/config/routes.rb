15587a1d12fe20062882868eb965c8c11fe0f3cb
日下陸矢 <rikuya.98@gmail.com>
2024-07-27 14:25:53 +0900

    routesにcommentsのルーティングを追加

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :tests, only: %i[index]
-  resources :memos, only: %i[index show create update destroy]
+  resources :memos, only: %i[index show create update destroy] do
+    resources :comments, only: %i[create]
+  end
 end
