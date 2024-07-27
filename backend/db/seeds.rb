1f90a9694beeb87be8b7aa328e28c17c8db49daa
ochi-sho-private-study <118090073+ochi-sho-private-study@users.noreply.github.com>
2024-07-27 14:37:03 +0900

    Update backend/db/seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -21,8 +21,8 @@ require "faker"
         
   10.times do |m|
     Comment.create!(
-    memo_id: memo.id,
-    content:  Faker::Lorem.sentence(word_count:5)
+      memo_id: memo.id,
+      content:  Faker::Lorem.sentence(word_count:5)
     )            
   end
 end
