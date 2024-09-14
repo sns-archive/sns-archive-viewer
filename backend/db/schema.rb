e46c0f9486bd19170c12e8a161e951f03ebbc0b9
Nine-777 <hkearth777@gmail.com>
2024-09-14 15:53:00 +0900

    メモのschemaと関連modelの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -19,19 +19,40 @@ ActiveRecord::Schema[7.0].define(version: 0) do
     t.index ["memo_id"], name: "index_comments_on_memo_id"
   end
 
+  create_table "memo_tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "memo_id", null: false, comment: "メモID"
+    t.bigint "tag_id", null: false, comment: "タグID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["memo_id", "tag_id"], name: "unique_index_memo_id_and_tag_id_on_memo_tags", unique: true
+    t.index ["memo_id"], name: "index_memo_tags_on_memo_id"
+    t.index ["tag_id"], name: "index_memo_tags_on_tag_id"
+  end
+
   create_table "memos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title", null: false, comment: "メモのタイトル"
+    t.string "title", limit: 30, null: false, comment: "メモのタイトル"
     t.text "content", null: false, comment: "メモの本文"
     t.timestamp "created_at", null: false
     t.timestamp "updated_at", null: false
   end
 
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "label", limit: 30, null: false, comment: "ラベル"
+    t.integer "color", null: false, comment: "色"
+    t.float "priority", null: false, comment: "優先度"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["label"], name: "unique_index_label_on_tags", unique: true
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", null: false, comment: "ユーザーのEmailアドレス"
-    t.string "password", null: false, comment: "ユーザーのpassword"
+    t.string "email", limit: 30, null: false, comment: "ユーザーのEmailアドレス"
+    t.string "password", limit: 30, null: false, comment: "ユーザーのpassword"
     t.timestamp "created_at", null: false
     t.timestamp "updated_at", null: false
   end
 
   add_foreign_key "comments", "memos", name: "fk_comments_memo_id"
+  add_foreign_key "memo_tags", "memos", name: "fk_memo_id_on_memo_tags"
+  add_foreign_key "memo_tags", "tags", name: "fk_tag_id_on_memo_tags"
 end
