595b3b79261557849c9983a7eb46789c790634ac
LocalhostTI <takumi.localhost@gmail.com>
2025-04-06 12:10:13 +0900

    is_publicのコメント修正

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 0) do
 
   create_table "channels", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", limit: 50, null: false, comment: "チャンネル名"
-    t.boolean "is_public", null: false, comment: "公開・非公開"
+    t.boolean "is_public", null: false, comment: "公開(true)・非公開(false)の設定"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- backend/spec/models/channel_spec.rb
+++ backend/spec/models/channel_spec.rb
@@ -4,11 +4,11 @@
 #
 # Table name: channels
 #
-#  id                      :bigint           not null, primary key
-#  is_public(公開・非公開) :boolean          not null
-#  name(チャンネル名)      :string(50)       not null
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
+#  id                                         :bigint           not null, primary key
+#  is_public(公開(true)・非公開(false)の設定) :boolean          not null
+#  name(チャンネル名)                         :string(50)       not null
+#  created_at                                 :datetime         not null
+#  updated_at                                 :datetime         not null
 #
 
 RSpec.describe Channel do
