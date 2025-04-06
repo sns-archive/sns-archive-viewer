170456d79bb6e3f34fa442b9bc7f4bd63fd24792
LocalhostTI <takumi.localhost@gmail.com>
2025-04-06 12:14:59 +0900

    rubocop対応

--- backend/app/models/memo/update_form.rb
+++ backend/app/models/memo/update_form.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class Memo
   class UpdateForm
     include ActiveModel::Validations
@@ -10,21 +11,21 @@ class Memo
     # @param params [ActionController::Parameters] Controller側でpermitしたパラメータ
     #   @option params [String] :content メモの本文
     # @return [Memo::UpdateForm]
-    def self.build(params,id)
+    def self.build(params, id)
       new(params, id).tap(&:setup)
     end
 
     # e.g. params = { memo: { id: 1, content: 'メモ更新' } }
-    def initialize(params,id)
+    def initialize(params, id)
       @params = params
       @id = id
     end
 
     def setup
       @memo = Memo.find(@id)
-      unless @params[:content].nil?
-        @memo.content = @params[:content]
-      end
+      return if @params[:content].nil?
+
+      @memo.content = @params[:content]
     end
 
     # @return [Boolean] 保存に成功したかどうか
